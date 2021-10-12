1b26defb7f067323cec80dce765c4fa122f77f39
Wang <tianshu.a.wang@accenture.com>
2021-10-12 19:26:12 -0300

    add oauth2 client_credentials for the API secutiry

--- src/main/java/com/tianshu/SpringMongodbApplication.java
+++ src/main/java/com/tianshu/SpringMongodbApplication.java
@@ -5,9 +5,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
 @EnableMongoRepositories
+@EnableAuthorizationServer
 public class SpringMongodbApplication {
 
 	@Autowired
---
+++ src/main/java/com/tianshu/config/WebSecurityConfig.java
@@ -0,0 +1,47 @@
+package com.tianshu.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+@Order(SecurityProperties.BASIC_AUTH_ORDER)
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Value("${user.oauth.user.username}")
+    private String username;
+
+    @Value("${user.oauth.user.password}")
+    private String password;
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Bean
+    @Override
+    public UserDetailsService userDetailsService() {
+
+        UserDetails user = User.builder().username(username).password(passwordEncoder().encode(password)).
+                roles("USER").build();
+
+        return new InMemoryUserDetailsManager(user);
+    }
+
+}
